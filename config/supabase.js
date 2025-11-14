5b396045fe6fd7d8cc6111d3d686f46ff4774ad0
Júlia <Julia@MacBook-Pro-de-Alisson.local>
2025-11-13 22:02:34 -0300

    atualizacao de key supabase

--- config/supabase.js
+++ config/supabase.js
@@ -1,7 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
 const SUPABASE_URL = 'https://bcvafvwfzbfohxrvwcuy.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjdmFmdndmemJmb2h4cnZ3Y3V5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MjM3MDE1MywiZXhwIjoyMDc3OTQ2MTUzfQ.99oaFHSli1aj5a7nUc6z7tOaT_t6QpPS1aBdr_vFxrc';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjdmFmdndmemJmb2h4cnZ3Y3V5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzNzAxNTMsImV4cCI6MjA3Nzk0NjE1M30.aS1scdS8kUTDGmJgrzirSwcYN6BAQypn1BMclP0hD6g';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
